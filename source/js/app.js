1d0d202138608e9111250dfab27a9659f97a1c88
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-11-27 12:15:56 +0100

    add principal players from list players

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,24 @@
 // varibales
 let players;
 let cardAllPlayer;
+let principalPlayer;
 
-const cardPlayer = document.querySelectorAll('.card-player');
+const stadiumImage = document.querySelector('.stadium-image')
 const modalPlayers = document.querySelector('.modal-players');
 const close = document.querySelector('.close');
 
 const allPlayers = document.querySelector('.allPlayers');
 
-
 // event click to show all players
-cardPlayer.forEach(card => {
-    card.addEventListener('click', () => {
-        modalPlayers.classList.toggle('hidden')
+const showModalPlayers = () => {
+    const appPlayer = document.querySelectorAll('.add-player');
+    appPlayer.forEach(card => {
+        card.addEventListener('click', () => {
+            
+            modalPlayers.classList.toggle('hidden')
+        })
     })
-})
+}
 
 close.addEventListener('click', () => {
     modalPlayers.classList.remove('hidden')
@@ -28,6 +32,23 @@ fetch('/source/players.json')
 
     let localPlayers = JSON.parse(localStorage.getItem('playersSelected')) || [];
 
+    const findPositionPlayer = (positionPlayer, playerId) => {
+
+        const findPosition = localPlayers.filter(player => player.position === positionPlayer);
+        
+        const playerIndex = findPosition.findIndex(player => player.id === playerId);
+        
+
+        if (playerIndex === 0) {
+            return `${positionPlayer}`;
+        } else if (playerIndex === 1) {
+            return `l-${positionPlayer}`;
+        } else {
+            return `r-${positionPlayer}`;
+        }
+    };
+    
+    showPrincipalPlayers()
     showAllPlayers()
 
     // function for show all players
@@ -85,7 +106,7 @@ fetch('/source/players.json')
             player.addEventListener('click', () => {
                 const playerId = players.find(p => p.id == cardId)
 
-                const findInLocal = localPlayers.find(p => p.id == cardId)
+                const findInLocal = localPlayers.find(p => p.id == cardId && p.position)
 
                 if(findInLocal) {
                     return;
@@ -94,8 +115,62 @@ fetch('/source/players.json')
                 if(playerId) {
                     localPlayers.push(playerId)
                     localStorage.setItem('playersSelected', JSON.stringify(localPlayers))
-                }  
+                    modalPlayers.classList.remove('hidden')
+                    
+                    showPrincipalPlayers();
+                }
             })
         })
+    }
+
+    // show principal players
+    function showPrincipalPlayers() {
+        JSON.parse(localStorage.getItem('playersSelected')) || [];
+        
+        localPlayers.map(player => {
+            principalPlayer =
+                `<div class="card-player ${findPositionPlayer(player.position, player.id)}" data-id="${player.id}">
+                    <div class="head-card">
+                        <div class="position">
+                            <p>${player.rating}</p>
+                            <span>${player.position}</span>
+                        </div>
+                        <div class="image-player">
+                            <img width="100" src="${player.photo}" alt="">
+                        </div>
+                    </div>
+                    <div class="body-card">
+                        <h3>${player.name}</h3>
+                        <div class="rate-player">
+                            <div>
+                                <p>PAC</p>
+                                <span>${player.pace || player.diving}</span>
+                            </div>
+                            <div>
+                                <p>SHO</p>
+                                <span>${player.shooting || player.handling}</span>
+                            </div>
+                            <div>
+                                <p>PAS</p>
+                                <span>${player.passing || player.kicking}</span>
+                            </div>
+                            <div>
+                                <p>DRI</p>
+                                <span>${player.dribbling || player.reflexes}</span>
+                            </div>
+                            <div>
+                                <p>DEF</p>
+                                <span>${player.defending || player.speed}</span>
+                            </div>
+                            <div>
+                                <p>PHY</p>
+                                <span>${player.physical || player.positioning}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+            stadiumImage.innerHTML += principalPlayer;
+        })
+        showModalPlayers();
     }
 })
