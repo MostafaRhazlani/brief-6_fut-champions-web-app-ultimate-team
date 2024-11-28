0d5bdf9b5187aa2b1763599ff5200c40a4ee5341
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-11-28 11:56:07 +0100

    add selected player to substitutes from list players

--- source/js/app.js
+++ source/js/app.js
@@ -2,8 +2,10 @@
 let players;
 let cardAllPlayer;
 let principalPlayer;
+let substitutePlayer;
 
-const stadiumImage = document.querySelector('.stadium-image')
+const stadiumImage = document.querySelector('.stadium-image');
+const substitutes = document.querySelector('.substitutes');
 const modalPlayers = document.querySelector('.modal-players');
 const close = document.querySelector('.close');
 
@@ -15,7 +17,16 @@ const showModalPlayers = () => {
     appPlayer.forEach(card => {
         card.addEventListener('click', () => {
             
-            modalPlayers.classList.toggle('hidden')
+            if(card.dataset.card == 1) {
+                console.log(card.dataset.card);
+                
+                allPlayers.dataset.id = card.dataset.card
+                modalPlayers.classList.toggle('hidden')
+            } else {
+                console.log(card.dataset.card);
+                allPlayers.dataset.id = card.dataset.card
+                modalPlayers.classList.toggle('hidden')  
+            }
         })
     })
 }
@@ -31,6 +42,7 @@ fetch('/source/players.json')
     players = res.players
 
     let localPlayers = JSON.parse(localStorage.getItem('playersSelected')) || [];
+    let localSubstitutes = JSON.parse(localStorage.getItem('substitutes')) || [];
 
     const findPositionPlayer = (positionPlayer, playerId) => {
 
@@ -49,13 +61,14 @@ fetch('/source/players.json')
     };
     
     showPrincipalPlayers()
+    showSubstitutesPlayers()
     showAllPlayers()
 
     // function for show all players
     function showAllPlayers() {
         players.map(player => {
             cardAllPlayer = 
-            `<div class="card-player selected-player" data-id="${player.id}">
+            `<div class="card-player selected-player select-substitute-player" data-id="${player.id}">
                 <div class="head-card">
                     <div class="position">
                         <p>${player.rating}</p>
@@ -106,23 +119,29 @@ fetch('/source/players.json')
         
         document.querySelectorAll('.selected-player').forEach(player => {
             const cardId = player.dataset.id
-            
+
             player.addEventListener('click', () => {
                 const playerId = players.find(p => p.id == cardId)
 
-                const findInLocal = localPlayers.find(p => p.id == cardId && p.position)
+                const findInLocal = localPlayers.find(p => p.id == cardId)
 
                 if(findInLocal) {
                     return;
                 }
 
                 if(playerId) {
-                    localPlayers.push(playerId)
-                    localStorage.setItem('playersSelected', JSON.stringify(localPlayers))
-                    modalPlayers.classList.remove('hidden')
+                    if(allPlayers.dataset.id == 1) {
+                        localPlayers.push(playerId)
+                        localStorage.setItem('playersSelected', JSON.stringify(localPlayers))
+                        showPrincipalPlayers();
+                    } else {
+                        localSubstitutes.push(playerId)
+                        localStorage.setItem('substitutes', JSON.stringify(localSubstitutes))
+                        showSubstitutesPlayers();
+                    }
                     
-                    showPrincipalPlayers();
                 }
+                modalPlayers.classList.remove('hidden')
             })
         })
     }
@@ -182,4 +201,60 @@ fetch('/source/players.json')
         })
         showModalPlayers();
     }
+
+    function showSubstitutesPlayers() {
+        JSON.parse(localStorage.getItem('substitutes')) || [];
+        
+        localSubstitutes.map(player => {
+            substitutePlayer =
+                `<div class="card-player" data-id="${player.id}">
+                    <div class="head-card">
+                        <div class="position">
+                            <p>${player.rating}</p>
+                            <span>${player.position}</span>
+                        </div>
+                        <div class="image-player">
+                            <img width="150" src="${player.photo}" alt="">
+                        </div>
+                    </div>
+                    <div class="body-card">
+                        <h3>${player.name}</h3>
+                        <div class="rate-player">
+                            <div>
+                                <p>PAC</p>
+                                <span>${player.pace || player.diving}</span>
+                            </div>
+                            <div>
+                                <p>SHO</p>
+                                <span>${player.shooting || player.handling}</span>
+                            </div>
+                            <div>
+                                <p>PAS</p>
+                                <span>${player.passing || player.kicking}</span>
+                            </div>
+                            <div>
+                                <p>DRI</p>
+                                <span>${player.dribbling || player.reflexes}</span>
+                            </div>
+                            <div>
+                                <p>DEF</p>
+                                <span>${player.defending || player.speed}</span>
+                            </div>
+                            <div>
+                                <p>PHY</p>
+                                <span>${player.physical || player.positioning}</span>
+                            </div>
+                            </div>
+                            <div class="current-team">
+                                <img width="17" height="10" src="${player.flag}" alt="">
+                                <img width="14" src="${player.logo}" alt="">
+                            </div>
+                    </div>
+                    
+                </div>`
+                substitutes.innerHTML += substitutePlayer;
+        })
+        showModalPlayers();
+    }
+    showModalPlayers();
 })
